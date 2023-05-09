a1454ff789ddc4f8d0e4f16925c2c1166ee0d6d8
unknown <khulood.hassan@hotmail.com>
2023-05-09 08:55:02 +0400

    Deploy with cyclic

--- app.js
+++ app.js
@@ -733,7 +733,7 @@ app.get('/api/:quoteID', (req,res)=>{
 
 app.get('/api/:random', (req,res)=>{
     const random = Math.floor(Math.random() * 117) + 1
-    res.json(quotes[random.toString()])
+    res.json(quotes[random.toString().padStart(3,'0')])
 })
 
 app.get('/docs', (req, res) => {
