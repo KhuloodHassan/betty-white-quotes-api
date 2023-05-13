89a8f6def2c1d13d6cf1f55ad11c93c519cb63d6
unknown <khulood.hassan@hotmail.com>
2023-05-13 21:01:34 +0400

    Final commit

--- public/js/theme-toggle.js
+++ public/js/theme-toggle.js
@@ -1,18 +1,19 @@
 let isLight = true
+
 const html = document.documentElement
-const switchTheme = document.getElementById('theme-toggle')
-const os_default = '<i class="bx bxs-moon bx-md"></i>'
+const themeToggle = document.getElementById('theme-toggle')
+
 const sun = '<i class="bx bxs-sun bx-md"></i>'
 const moon = '<i class="bx bxs-moon bx-md"></i>'
 
-document.addEventListener('DOMContentLoaded', () => {
-  switchTheme.innerHTML = os_default
-  html.setAttribute('data-theme', 'dark')
+document.addEventListener('DOMContentLoaded', _ => {
+    themeToggle.innerHTML = moon
+    html.setAttribute('data-theme', 'dark')
 })
 
-switchTheme.addEventListener('click', (e)=> {
-  e.preventDefault()
-  isLight = !isLight
-  html.setAttribute('data-theme', isLight? 'dark':'light')
-  switchTheme.innerHTML = isLight? moon : sun
+themeToggle.addEventListener('click', e => {
+    e.preventDefault()
+    isLight = !isLight
+    html.setAttribute('data-theme', isLight ? 'dark' : 'light')
+    themeToggle.innerHTML = isLight ? moon : sun
 })
